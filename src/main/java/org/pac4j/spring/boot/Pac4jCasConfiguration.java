d1774ea507cf1d45579a238c41fd87cb538cc9fd
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:06:30 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.3.x branch

--- src/main/java/org/pac4j/spring/boot/Pac4jCasConfiguration.java
+++ src/main/java/org/pac4j/spring/boot/Pac4jCasConfiguration.java
@@ -1,6 +1,6 @@
 package org.pac4j.spring.boot;
 
-import org.apereo.cas.client.util.CommonUtils;
+import org.jasig.cas.client.util.CommonUtils;
 import org.pac4j.cas.client.CasClient;
 import org.pac4j.cas.client.CasProxyReceptor;
 import org.pac4j.cas.client.direct.DirectCasClient;
@@ -8,8 +8,10 @@ import org.pac4j.cas.client.direct.DirectCasProxyClient;
 import org.pac4j.cas.client.rest.CasRestBasicAuthClient;
 import org.pac4j.cas.client.rest.CasRestFormClient;
 import org.pac4j.cas.config.CasConfiguration;
+import org.pac4j.core.context.JEEContext;
 import org.pac4j.core.http.callback.CallbackUrlResolver;
 import org.pac4j.core.http.url.UrlResolver;
+import org.pac4j.core.logout.handler.LogoutHandler;
 import org.pac4j.spring.boot.utils.Pac4jUrlUtils;
 import org.pac4j.spring.boot.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,7 +45,7 @@ public class Pac4jCasConfiguration {
 	}
 	
 	@Bean
-	public CasConfiguration casConfiguration(UrlResolver urlResolver,
+	public CasConfiguration casConfiguration(LogoutHandler<JEEContext> logoutHandler, UrlResolver urlResolver,
 			@Autowired(required = false) CasProxyReceptor proxyReceptor) {
 
 		CasConfiguration configuration = new CasConfiguration(pac4jCasProperties.getLoginUrl(), pac4jCasProperties.getProtocol() );
@@ -62,6 +64,7 @@ public class Pac4jCasConfiguration {
 		map.from(pac4jCasProperties.getCustomParams()).to(configuration::setCustomParams);
 		map.from(pac4jCasProperties.isGateway()).to(configuration::setGateway);
 		map.from(pac4jCasProperties.getLoginUrl()).to(configuration::setLoginUrl);
+		map.from(logoutHandler).to(configuration::setLogoutHandler);
 		map.from(pac4jCasProperties.getMethod()).to(configuration::setMethod);
 		map.from(pac4jCasProperties.getPostLogoutUrlParameter()).to(configuration::setPostLogoutUrlParameter);
 		map.from(pac4jCasProperties.getPrefixUrl()).to(configuration::setPrefixUrl);
@@ -78,7 +81,7 @@ public class Pac4jCasConfiguration {
 	/**
 	 * 
 	 * TODO
-	 * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+	 * @author [@Loong Wan](https://github.com/loong10k)
 	 * @param configuration
 	 * @return
 	 */
